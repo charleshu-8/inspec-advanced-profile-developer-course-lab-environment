61836db3dba203608e9980dc279617d039073246
Charles Hu <106095667+charleshu-8@users.noreply.github.com>
2022-06-15 15:05:41 +0000

    Work

--- filtertable-test/controls/example.rb
+++ filtertable-test/controls/example.rb
@@ -6,3 +6,8 @@ describe filtertable.where( name: "Sarah" ) do
   it { should have_car }  
   it { should be_girl } 
 end  
+
+describe filtertable do
+  its('girls_count') { should cmp 1 }
+  its('boys_count') { should cmp 2 }
+end  
